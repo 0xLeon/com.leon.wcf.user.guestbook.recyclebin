1e5c943799df012297c753c4bb2af88904793b23
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-06-26 20:46:58 +0200

    added four columns: deleteTime, deletedBy, deletedByID and deleteReason

--- install.sql
+++ install.sql
@@ -1,1 +1,5 @@
 ALTER TABLE wcf1_user_guestbook ADD COLUMN isDeleted TINYINT(1) NOT NULL DEFAULT 0 AFTER time;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN deleteTime INT(10) NOT NULL DEFAULT 0 AFTER isDeleted;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN deletedBy VARCHAR(255) NOT NULL DEFAULT '' AFTER deleteTime;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN deletedByID INT(10) NOT NULL DEFAULT 0 AFTER deletedBy;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN deleteReason TEXT AFTER deletedByID;
