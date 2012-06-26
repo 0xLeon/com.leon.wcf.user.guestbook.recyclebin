abe43a71b4a68a2b6a035277275e8eb504346756
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-06-26 19:57:41 +0200

    put entry in recycle bin on deletion if recycle bin is enabled and entry isn't in recycle bin already

--- files/lib/system/event/listener/UserGuestbookEntryDeleteActionRecycleBinListener.class.php
+++ files/lib/system/event/listener/UserGuestbookEntryDeleteActionRecycleBinListener.class.php
@@ -17,6 +17,16 @@ class UserGuestbookEntryDeleteActionRecycleBinListener implements EventListener 
 	 * @see EventListener::execute()
 	 */
 	public function execute($eventObj, $className, $eventName) {
-		
+		if (GUESTBOOK_ENABLE_RECYCLE_BIN && !$eventObj->entry->isDeleted) {
+			$sql = "UPDATE	wcf".WCF_N."_user_guestbook
+				SET	isDeleted = 1
+				WHERE	entryID = ".$eventObj->entry->entryID;
+			WCF::getDB()->sendQuery($sql);
+			
+			$eventObj->executed();
+			
+			HeaderUtil::redirect('index.php?page=UserGuestbook&userID='.$eventObj->entry->ownerID.'&entryID='.$eventObj->entry->entryID.SID_ARG_2ND_NOT_ENCODED.'#entry'.$eventObj->entry->entryID);
+			exit;
+		}
 	}
 }
