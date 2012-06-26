38f9c1fe30fc57c55e34bfd5d8c2404d1c01c713
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-06-26 20:55:03 +0200

    added columns for comment recycle bin

--- install.sql
+++ install.sql
@@ -3,3 +3,9 @@ ALTER TABLE wcf1_user_guestbook ADD COLUMN deleteTime INT(10) NOT NULL DEFAULT 0
 ALTER TABLE wcf1_user_guestbook ADD COLUMN deletedBy VARCHAR(255) NOT NULL DEFAULT '' AFTER deleteTime;
 ALTER TABLE wcf1_user_guestbook ADD COLUMN deletedByID INT(10) NOT NULL DEFAULT 0 AFTER deletedBy;
 ALTER TABLE wcf1_user_guestbook ADD COLUMN deleteReason TEXT AFTER deletedByID;
+
+ALTER TABLE wcf1_user_guestbook ADD COLUMN commentIsDeleted TINYINT(1) NOT NULL DEFAULT 0 AFTER time;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN commentDeleteTime INT(10) NOT NULL DEFAULT 0 AFTER isDeleted;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN commentDeletedBy VARCHAR(255) NOT NULL DEFAULT '' AFTER deleteTime;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN commentDeletedByID INT(10) NOT NULL DEFAULT 0 AFTER deletedBy;
+ALTER TABLE wcf1_user_guestbook ADD COLUMN commentDeleteReason TEXT AFTER deletedByID;
