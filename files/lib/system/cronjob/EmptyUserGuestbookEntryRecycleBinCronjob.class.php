16a103394e865096505450e16b86728f4cc05bbe
Stefan Hahn <Stefan_Hahn1@gmx.net>
2012-06-27 02:19:30 +0200

    added function to empty recycle bin cron

--- files/lib/system/cronjob/EmptyUserGuestbookEntryRecycleBinCronjob.class.php
+++ files/lib/system/cronjob/EmptyUserGuestbookEntryRecycleBinCronjob.class.php
@@ -17,7 +17,41 @@ class EmptyRecycleBinCronjob implements Cronjob {
 	 */
 	public function execute($data) {
 		if (GUESTBOOK_ENABLE_RECYCLE_BIN && GUESTBOOK_EMPTY_RECYCLE_BIN_CYCLE > 0) {
+			// delete trashed entries
+			$sql = "SELECT	entryID
+				FROM	wcf".WCF_N."_user_guestbook
+				WHERE	isDeleted = 1
+					AND deleteTime < ".(TIME_NOW - GUESTBOOK_EMPTY_RECYCLE_BIN_CYCLE * 86400);
+			$result = WCF::getDB()->sendQuery($sql);
+			if (WCF::getDB()->countRows($result) > 0) {
+				$entryIDs = '';
+				while ($row = WCF::getDB()->fetchArray($result)) {
+					if (!empty($threadIDs)) $entryIDs .= ',';
+					$entryIDs .= $row['entryID'];
+				}
+				
+				$sql = "DELETE
+					FROM	wcf".WCF_N."_user_guestbook
+					WHERE	entryID in (".$entryIDs.")";
+				WCF::getDB()->registerShutdownQuery($sql);
+				
+				$sql = "DELETE
+					FROM	wcf".WCF_N."_user_guestbook_hash
+					WHERE	entryID in (".$entryIDs.")";
+				WCF::getDB()->registerShutdownQuery($sql);
+			}
 			
+			// deleted trashed entry comments
+			$sql = "UPDATE	wcf".WCF_N."_user_guestbook
+				SET	comment = '',
+					commentTime = 0,
+					commentIsDeleted = 0,
+					commentDeleteTime = 0,
+					commentDeletedBy = '',
+					commentDeletedByID = 0
+				WHERE	commentIsDeleted = 1
+					AND commentDeleteTime < ".(TIME_NOW - GUESTBOOK_EMPTY_RECYCLE_BIN_CYCLE * 86400);
+			WCF::getDB()->registerShutdownQuery($sql);
 		}
 	}
 }
